9caa8e1b8df4ac01e18746e87b23ef7160d65126
Ariel <ariel.wm1@gmail.com>
2020-04-08 12:08:49 -0400

    add error handling: deleting

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -28,12 +28,17 @@ export class TasksService {
             );
         }
 
-
         return tasks;
     }
 
     getTaskById(id: string): Task{
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+
+        if(!found){ // non existing task
+            throw new NotFoundException(`Task whit ID "${id}" not found`);
+        }
+
+        return found;
     }
     
     createTask(createTaskDto: CreateTaskDto): Task {
